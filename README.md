f8d3b8d823853e62757a3906cb21dee55712f04d
MOROLARI <tmorolari@gmail.com>
2019-04-09 01:47:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Shazam-Test
---
+++ src/main/java/com/audioanalyzer/shazam/view/MainView.java
@@ -0,0 +1,13 @@
+package com.audioanalyzer.shazam.view;
+
+public class MainView {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AudioRecognizerWindow audioWindow = new AudioRecognizerWindow(
+				"Audio Recognizer");
+		audioWindow.createWindow();
+	}
+
+}
